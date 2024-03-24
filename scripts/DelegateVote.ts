a46687a721dc0de8d683688135f31a7be2e24867
Karlo Sebalj <karlo.sebalj@greyp.com>
2024-03-24 23:28:43 +0100

    Added DelegateVote script

---
+++ scripts/DelegateVote.ts
@@ -0,0 +1,56 @@
+import { sepolia } from "viem/chains";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { createPublicClient, http, createWalletClient, hexToString} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { abi } from "../artifacts/contracts/Ballot.sol/Ballot.json";
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+
+async function main() {
+  const parameters = process.argv.slice(2);
+  if (!parameters || parameters.length < 2)
+    throw new Error("Parameters not provided");
+  const contractAddress = parameters[0] as `0x${string}`;
+  if (!contractAddress) throw new Error("Contract address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+    throw new Error("Invalid contract address");
+  const delegateeAddress = parameters[1]; // Delegatee's Ethereum address
+  if (!delegateeAddress) throw new Error("Delegatee address not provided");
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+  const voter = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  console.log("Delegating votes to:", delegateeAddress);
+  console.log("Confirm? (Y/n)");
+  const stdin = process.openStdin();
+  stdin.addListener("data", async function (d) {
+    if (d.toString().trim().toLowerCase() != "n") {
+      const hash = await voter.writeContract({
+        address: contractAddress,
+        abi,
+        functionName: "delegate",
+        args: [delegateeAddress], // Pass the delegatee's address as an argument
+      });
+      console.log("Transaction hash:", hash);
+      console.log("Waiting for confirmations...");
+      const receipt = await publicClient.waitForTransactionReceipt({ hash });
+      console.log("Transaction confirmed");
+    } else {
+      console.log("Operation cancelled");
+    }
+    process.exit();
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
